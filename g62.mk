20cc6e6867900a50cca6ff12bdde07db7750d296
Yukarin <yukariin@yandex.ru>
2017-03-14 21:34:40 +0300

    Update

--- g62.mk
+++ g62.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/hp/g62/device.mk)
 
-PRODUCT_NAME := g62
+PRODUCT_NAME := aosp_g62
 PRODUCT_DEVICE := g62
 PRODUCT_BRAND := Android
 PRODUCT_MANUFACTURER := HP
